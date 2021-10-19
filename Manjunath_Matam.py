dc8153362b4ec880a99f15d66103567419776f03
Msquare <41698531+Matammanjunath@users.noreply.github.com>
2021-10-18 20:36:34 -0400

    submitted program and a txt file
    A few edits to the program, mainly related to the format, description, changing few variable names. Also, a txt file is included.

--- Manjunath_Matam.py
+++ Manjunath_Matam.py
@@ -2,16 +2,14 @@
 """
 Created on Tue Oct 12 22:12:00 2021
 
-Motivation: LBNL Programming assessment received from Dr. Anubhav Jain
-@author: Manjunath Matam (manjunath.matam@ucf.edu, manjunath031@gmail.com)
+Motivation: LBNL Programming assessment received from Dr. Anubhav Jain.
+@author: Dr. Manjunath Matam (manjunath.matam@ucf.edu, manjunath031@gmail.com)
 
 Objective: Take two files (files.txt and nodes.txt) through command line, process them, 
           distribute the files to appropriate nodes, and produce an output file (result.txt). 
 
-Objective: Take two files (files.txt and nodes.txt) through the command line, process them, 
-          distribute files to appropriate nodes, and produce an output file (result.txt). 
-
-Distribution principle: The author has implemented the Min-Max distribution plan to produce a solution to balance the data stored on the nodes, not the space on the nodes. 
+Distribution principle: The author has implemented the Min-Max distribution plan to produce 
+                        a solution to balance the data stored on the nodes, not the space on the nodes. 
 
 Two input files that adhere to the specifications mentioned in the document are: 
     1. Blank lines or lines starting with # character can be ignored in the process. 
@@ -32,9 +30,45 @@ Additional rules (not directly specified in the document but) considered by the 
 #### Import few standard modules
 import pandas as pd
 import numpy as np
-
 import argparse
 import sys
+from itertools import zip_longest
+
+
+## command line interface codes for importing input files
+parser = argparse.ArgumentParser(
+    usage="The program takes two input files (files.txt, nodes.txt) and produces an output file (result.txt) in three steps.",
+    description="Description: The program uses Min-Max distribution plan for allocating files to nodes.",
+)
+parser.add_argument(
+    "-f",
+    "-input-files",
+    type=str,
+    dest="ip_file",
+    required=True,
+    choices=["files.txt"],
+    help="Input the correct file named files.txt",
+)
+parser.add_argument(
+    "-n",
+    "-input-nodes",
+    type=str,
+    dest="ip_node",
+    required=True,
+    choices=["nodes.txt"],
+    help="Input the correct file named nodes.txt",
+)
+parser.add_argument(
+    "-o",
+    type=str,
+    default="result.txt",
+    required=False,
+    dest="op_result",
+    help="Provide a name to the output file (Default: result.txt)",
+)
+args = parser.parse_args()
+## Print the namesapce object
+print(args)
 
 
 def convert_txt2df(ip):
@@ -101,45 +135,6 @@ def df_processing(df):
 
 ### Main program begins
 if __name__ == "__main__":
-    # ### Uncomment following lines for debuggins
-    # ### Directly import the two input files
-    # fdf = convert_txt2df('files.txt')
-    # ndf = convert_txt2df('nodes.txt')
-
-    ## command line interface codes for importing input files
-    parser = argparse.ArgumentParser(
-        usage="Description: The program takes two input files (files.txt, nodes.txt) and produces an output file (result.txt) in three steps.",
-        description="Description: The program takes two input files (files.txt, nodes.txt) and produces an output file (result.txt) in three steps.",
-    )
-    parser.add_argument(
-        "-f",
-        "-input-files",
-        type=str,
-        dest="ip_file",
-        required=True,
-        choices=["files.txt"],
-        help="Input the correct file named files.txt",
-    )
-    parser.add_argument(
-        "-n",
-        "-input-nodes",
-        type=str,
-        dest="ip_node",
-        required=True,
-        choices=["nodes.txt"],
-        help="Input the correct file named nodes.txt",
-    )
-    parser.add_argument(
-        "-o",
-        type=str,
-        default="result.txt",
-        required=False,
-        dest="op_result",
-        help="Provide a name to the output file (Default: result.txt)",
-    )
-    args = parser.parse_args()
-    ## Print the namesapce object
-    print(args)
     ## convert the input file to dataframe
     print("\n######################################################")
     print("####### STEP1/3: Converting .txt document to dataframe:")
@@ -162,71 +157,62 @@ if __name__ == "__main__":
     ### Name the columns; it is easier to process the named columns
     fdf.columns = ["filename", "size"]
     ndf.columns = ["nodename", "space"]
-    # print("\n ### Files-dataframe first 5 rows \n")
-    # print(fdf.head(n=5))
-    # print("\n ### Nodes-dataframe first 5 rows \n")
-    # print(ndf.head(n=5))
 
     ### Process the files and nodes to produce the output file
     print("\n######################################################")
     print(
         "####### STEP3/3: Implementing the Min-Max distribution plan for balancing the data stored on the nodes:"
     )
-    ### compute total file size and node space
+    ###tfs, tns: compute total size of the files and nodes
     tfs = fdf["size"].sum(axis=0)
     tns = ndf["space"].sum(axis=0)
     ### Compare the data size with respect to available memory in nodes
     dif = tfs * 100 / tns
-    ### comparison whether all the data on files can/can't be stored on to the nodes
+    ### Compare whether all the data on files can/can't be stored on to the nodes
     comp = np.where(dif <= 100, "can", "cannot")
     print(
         "############## Compared to the available memory on all nodes (100%%), data size on the files is %d %%."
         % (dif)
     )
     print("############## Hence, all the data %s be stored in the nodes." % (comp))
-    ### Allocation principles:
+    ### Distribution Allocation principles:
     ### 1. When data size is bigger than available spce, start dropping the bigger data files and until the remaining all can be accommodated in the nodes
-    ### 2. When data is smaller than available space, accommodate equal size data into all the nodes
+    ### 2. When data is smaller than available space, accommodate equal size data into all the nodes using Min-Max (M2) algorithm
+    ### 3. Proposed M2 algorithm reports 100% distribution accuracy when all the nodes have more space than average file size (sizes of all files/number of files)
     fdf.sort_values("size", inplace=True, ignore_index=True, ascending=False)
     fdf["target_node"] = np.nan
     ndf.sort_values("space", inplace=True, ignore_index=True, ascending=False)
-    ### Calculate maximum node space - nmax
+    ### Calculate maximum node space: nmax
     ### namx puts limit on the maximum file size that can transferred to the node
     nmax = ndf["space"].max(axis=0)
-    ### identify the oversized sizes that cannot be accomodated in any nodes
+    ### Files eligibility test: identify the oversized sizes that cannot be accomodated in any nodes
     ### check if each file (row) can be transfered to the nodes
     fdf1 = fdf[(fdf["size"] < tns) & (fdf["size"] < nmax)].copy()
     if len(fdf1) > 0:
         ### Reset the files df index
         fdf1.reset_index(inplace=True, drop=True)
-        ### calculate min file size that will be transported
+        ### tmin: calculate min file size that will be transported
         tmin = int(fdf1["size"].min(axis=0))
-        ### check if each node has enough space to accommodate the prescribed transfer limit
+        ### Nodes eligibility test: check if each node has enough space to accommodate the prescribed transfer limit
         ndf1 = ndf[ndf["space"] > tmin]
         if len(ndf1) > 0:
             print(
                 "############## Oversized files or undersized nodes have been ignored."
             )
-            ### calculate avg file size that can be transported to each node
+            ### tavg: calculate avg file size that can be transported to each node
             tavg = int(fdf1["size"].sum(axis=0) / len(ndf1))
-
-            ###
+            ### nf, nn: number of files and nodes that passed the eligibility test
             nf = len(fdf1)
             nn = len(ndf1)
-
-            lst = list(range(0, nf, 1))
-            flist = [lst[i : i + nn : 1] for i in range(0, nf, nn)]
-
-            # build new list where the sub-list are reversed if in odd indices
+            ### fidx: files index, 0,1,2,..,nf; 0 denotes big size file,..,nf denotes small size file
+            fidx = list(range(0, nf, 1))
+            flist = [fidx[i : i + nn : 1] for i in range(0, nf, nn)]
+            ### reflist: new list where every odd list elements are reversed
             revflist = [lst[::-1] if i % 2 == 1 else lst for i, lst in enumerate(flist)]
-
-            # zip using zip_longest and filter out the None values (the default fill value of zip_longest)
-            from itertools import zip_longest
-
+            ### solflist: Group the first elements in all sub lists in revflist by their position
             solflist = [
                 [v for v in vs if v is not None] for vs in zip_longest(*revflist)
             ]
-            # print(solflist)
 
             ### Allocate the files in the order of nodes
             for i in range(len(solflist)):
@@ -237,9 +223,10 @@ if __name__ == "__main__":
                 .combine_first(fdf1.set_index("filename"))
                 .reset_index()
             )
+            ### Replace NaN with NULL
             result.replace(np.nan, "NULL", inplace=True)
 
-            ### save the result
+            ### output: save the result; drop header while saving
             output = result[["filename", "target_node"]]
             output.to_csv("%s" % (args.op_result), header=False, index=False, sep=" ")
             print("\n######################################################")
@@ -247,7 +234,7 @@ if __name__ == "__main__":
                 "############## Distribution plan has been generated and saved as %s in the same folder."
                 % (args.op_result)
             )
-            ### cross check size of the file accommodated to each node and this is a correct distribution
+            ### chk: cross check each node's space > size of the file allocated to it and verify if it is a perfect distribution
             chk = result.groupby(by="target_node").sum()
             chk = pd.merge(
                 chk,
@@ -256,26 +243,22 @@ if __name__ == "__main__":
                 left_index=True,
                 right_index=True,
             )
+            ### Transferable?: True denotes the node has space to accommodate the allocated file(s)
             chk.loc[:, "Transferrable?"] = chk["size"] < chk["space"]
             chk.rename(
                 inplace=True, columns={"size": "File Size", "space": "Node Space"}
             )
             ### Distribution accuracy: how many of allocated nodes actually have space to accommodate the data
-            ### True positives
-            tp = len(
-                chk[(chk["Transferrable?"]) & ((chk["Node Space"] != np.nan))]
-            )
-            ### False positives
-            fp = len(
-                chk[(~chk["Transferrable?"]) & ((chk["Node Space"] == np.nan))]
-            )
-            ### accuracy
-            disacc = tp * 100 / (tp + fp)
+            ### True positives: node space > size of file allocated to it
+            tp = len(chk[(chk["Transferrable?"]) & ((chk["Node Space"] != np.nan))])
+            ### False positives: node space < size of file allocated to it
+            fp = len(chk[(~chk["Transferrable?"]) & ((chk["Node Space"] == np.nan))])
+            ### Distribution accuracy:
+            da = tp * 100 / (tp + fp)
             print(
                 "############## Also, the plan has been cross verified with %d%% accuracy."
-                % (disacc)
+                % (da)
             )
-            # print(chk.head()
         else:
             print("\n######################################################")
             print(
